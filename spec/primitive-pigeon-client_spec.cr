bcfd901189f52e45e2f8d2789976c160be589fa8
Ivan Chinenov <ichinenov@gmail.com>
2019-09-24 13:20:49 +0300

    initial commit

---
+++ spec/primitive-pigeon-client_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Primitive::Pigeon::Client do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/primitive-pigeon-client.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Primitive::Pigeon::Client`
+module Primitive::Pigeon::Client
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
